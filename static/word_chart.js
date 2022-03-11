ae2d3a4ff5bd9bdeae98c1a2959a4174f6093983
Michael Mondoro <michaelmondoro@Michaels-MacBook-Air.local>
2022-03-10 22:22:17 -0500

    updated error passing word query with Jinja

--- static/word_chart.js
+++ static/word_chart.js
@@ -1,56 +1,59 @@
-Chart.defaults.font.size = 16;
-Chart.defaults.elements.bar.borderWidth = 2;
+    
+function chart(word_query){
+    Chart.defaults.font.size = 16;
+    Chart.defaults.elements.bar.borderWidth = 2;
 
-var results = document.querySelectorAll('.meta');
-var books = [];
-var counts = [];
-var verses = [];
+    var results = document.querySelectorAll('.meta');
+    var books = [];
+    var counts = [];
+    var verses = [];
 
-for (const result of results.entries()){
-    rresult = result[1];
-    books = books.concat(rresult.getAttribute('book'));
-    counts = counts.concat(rresult.getAttribute('count'));
-    verses = verses.concat( [rresult.getAttribute('value').match(/[\w ]*/g) ]);
-    for (let i = 0; i<verses.length; i++) {
-        for (let j = 0; j<verses[i].length; j++){
-            if (verses[i][j].length < 2) {
-                verses[i].splice(j,1);
+    for (const result of results.entries()){
+        rresult = result[1];
+        books = books.concat(rresult.getAttribute('book'));
+        counts = counts.concat(rresult.getAttribute('count'));
+        verses = verses.concat( [rresult.getAttribute('value').match(/[\w ]*/g) ]);
+        for (let i = 0; i<verses.length; i++) {
+            for (let j = 0; j<verses[i].length; j++){
+                if (verses[i][j].length < 2) {
+                    verses[i].splice(j,1);
+                }
             }
         }
     }
+
+
+    const data = {
+        click: function(e){
+        alert(":LKJ");
+        },
+        labels: books,
+        datasets: [{
+        label: word_query,
+        backgroundColor: 'rgba(135, 206, 250,.4)',
+        borderColor: 'rgba(0, 100, 200,.5)',
+        data: counts,
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+            onClick: (event, activeElements) => {
+            if(activeElements.length > 0){
+                var indx = activeElements[0].index;
+                var val = books[indx];
+            
+                document.location.href = `results?book=${val}&values=${verses[indx]}&search_query=${word_query}`;
+            }
+            
+        },
+            
+        },
+    };
+
+    const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+    );
 }
-
-
-const data = {
-    click: function(e){
-    alert(":LKJ");
-    },
-    labels: books,
-    datasets: [{
-    label: '{{word_query}}',
-    backgroundColor: 'rgba(135, 206, 250,.4)',
-    borderColor: 'rgba(0, 100, 200,.5)',
-    data: counts,
-    }]
-};
-const config = {
-    type: 'bar',
-    data: data,
-    options: {
-        onClick: (event, activeElements) => {
-        if(activeElements.length > 0){
-            var indx = activeElements[0].index;
-            var val = books[indx];
-        
-            document.location.href = `results?book=${val}&values=${verses[indx]}&search_query={{word_query}}`;
-        }
-        
-    },
-        
-    },
-};
-
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-);
